d4300fc8082fea9328cfa1492ef120ae867dd725
ICavlek <ivan.cavlek1403@gmail.com>
2025-02-04 13:41:07 +0100

    feat: created parsing of program file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use provable_vm::provable_vm::program_loader::load_program;
+
 fn main() {
-    println!("Hello, world!");
+    let program = load_program("./examples/program.prov").expect("Failed to load program");
+    println!("{:#?}", program);
 }
