2c50be334bb44fde8dc66ce8a80e7750bac377c7
ICavlek <ivan.cavlek1403@gmail.com>
2025-02-04 19:39:33 +0100

    feat: groth16 in binary

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use ark_bls12_381::Bls12_381;
+use ark_groth16::Groth16;
+use ark_snark::CircuitSpecificSetupSNARK;
 use provable_vm::provable_vm::{
     circuit::ExecutionCircuit, program_loader::load_program, vm::ProvableVM,
 };
+use rand_chacha::ChaCha20Rng;
+use rand_core::SeedableRng;
 
 fn main() {
     let program = load_program("./examples/program.prov").expect("Failed to load program");
@@ -18,5 +23,8 @@ fn main() {
         trace_commitment,
     };
 
-    println!("{:#?}", circuit);
+    let mut rng = ChaCha20Rng::from_entropy();
+    let (pk, vk) = Groth16::<Bls12_381>::setup(circuit.clone(), &mut rng).unwrap();
+    println!("Proving key: {:#?}", pk);
+    println!("Verifying key: {:#?}", vk);
 }
